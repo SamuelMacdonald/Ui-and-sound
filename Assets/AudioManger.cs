797c1cfbd83293fc105468e5e544c6d53214a971
SamuelMacdonald <146819235+SamuelMacdonald@users.noreply.github.com>
2024-02-27 16:03:21 +0000

    tha

---
+++ Assets/AudioManger.cs
@@ -0,0 +1,25 @@
+
+using UnityEngine;
+
+public class AudioManger : MonoBehaviour
+{
+    [Header("Sources")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("Sounds")]
+    public AudioClip click;
+    public AudioClip music;
+
+    private void Start()
+    {
+        musicSource.clip = music;
+        musicSource.Play();
+    }
+
+    public void PlaySFX()
+    {
+        SFXSource.clip = click;
+        SFXSource.Play();
+    }
+}
